6c7959ca3ea17c29ea8cb9b42c011a6c06634fbd
naben-za <nabil.piscine42@gmail.com>
2021-08-26 12:06:47 +0200

    new gnl

--- gnl/gnl_new/get_next_line.c
+++ gnl/gnl_new/get_next_line.c
@@ -20,7 +20,6 @@ void	refresh_buf(char *b)
 	size_t	i;
 
 	i = 0;
-//	j = 0;
 	j = ft_strlen(b);
 	if (b[j] == '\n')
 		++j;
@@ -36,7 +35,6 @@ char	*get_next_line(int fd)
 	static char	buf[BUFFER_SIZE + 1];
 	int			rbytes;
 	char		*line;
-//	int			i;
 
 	if (BUFFER_SIZE <= 0 || read(fd, buf, 0) == -1)
 		return (NULL);
@@ -46,9 +44,6 @@ char	*get_next_line(int fd)
 	while (rbytes > 0 && !lookfor_nl(buf))
 	{
 		rbytes = read(fd, buf, BUFFER_SIZE);
-//		printf("buf1: -%s- ", buf);
-//		buf[rbytes] = 0;
-//		printf("rbytes: -%d-\n", rbytes);
 		if (rbytes)
 		{
 			buf[rbytes] = 0;
@@ -67,70 +62,7 @@ char	*get_next_line(int fd)
 			line = NULL;
 			return (line);
 		}
-		printf("line: -%s-\n", line);
 	}
-//	printf("buf before:%s", buf);
-/*	if (lookfor_nl(buf))
-	{
-		i = ft_strlen(line);
-		line[i] = '\n';
-	}*/
 	refresh_buf(buf);
-	printf("buf after refresh: -%s-\n", buf);
-//	printf("buf after:%s", buf);
-/*	if (!lookfor_nl(buf))
-	{
-		i = ft_strlen(line);
-		line[i] = 0;
-	}*/
-//	printf("%d\n", rbytes);
-/*	if (rbytes == 0)
-	{
-		free(line);
-		line = NULL;
-		return (line);
-	}*/
-//	printf("%d", rbytes);
 	return (line);
-}
-
-int main()
-{
-	int 	ret;
-	char	*line;
-	
-	line = "";
-	ret = open("test1.txt", O_RDONLY);
-		line = get_next_line(ret);
-		printf("main : %s", line);
-		free(line);
-		line = get_next_line(ret);
-		printf("main : %s", line);
-		free(line);
-		line = get_next_line(ret);
-		printf("main : %s", line);
-		free(line);
-/*		line = get_next_line(ret);
-		printf("main : %s", line);
-		free(line);
-		line = get_next_line(ret);
-		printf("main : %s", line);
-		free(line);
-		line = get_next_line(ret);
-		printf("main : %s", line);
-		free(line);
-		line = get_next_line(ret);
-		printf("main : %s", line);
-		free(line);
-		line = get_next_line(ret);
-		printf("main : %s", line);
-		free(line);*/
-
-//	while (line != NULL)
-//	{
-//		line = get_next_line(ret);
-//		printf("main %s", line);
-//		free(line);
-//	}
-//	return (0);
 }
