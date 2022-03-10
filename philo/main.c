2201ce28757806db204f9d759e8227ecc22f8bb1
naben-za <nabil.piscine42@gmail.com>
2022-03-10 22:07:35 +0100

    philo

--- philo/main.c
+++ philo/main.c
@@ -9,7 +9,7 @@ void    *start(void *arg)
     pthread_mutex_lock(&t->mutex);
     //while (++i < t->nb_philo)
     //{
-        usleep(2000000);
+        usleep(1000000);
         printf("philo %d attend\n", t->philo[i].philo_n);
         ++i;
     //}
@@ -26,22 +26,21 @@ int main(int ac, char **av)
     else
         args_nbr_error();
     pthread_mutex_init(&args.mutex, NULL);
-    args.philo = malloc(sizeof(args.philo) * args.nb_philo);
+    args.philo = malloc(sizeof(t_philo) * args.nb_philo);
     i = -1;
     while (++i < args.nb_philo)
     {
         args.philo[i].philo_n = i + 1;
         pthread_create(&args.philo[i].th, NULL, &start, &args);
-        pthread_mutex_init(&args.philo[i].mutex1, NULL);
         printf("thread %d executing\n", i + 1);
     }
     i = -1;
     while (++i < args.nb_philo)
     {
         pthread_join(args.philo[i].th, NULL);
-        pthread_mutex_destroy(&args.philo[i].mutex1);
         printf("thread %d has finished execution\n", i + 1);
     }
     pthread_mutex_destroy(&args.mutex);
+    free(args.philo);
     return (0);
 }
