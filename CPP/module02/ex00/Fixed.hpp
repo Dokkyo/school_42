267ba6c8f90a676f629ea398304093761f706853
naben-za <nabil.piscine42@gmail.com>
2022-06-20 10:42:42 +0200

    CPP Module02 in process

---
+++ CPP/module02/ex00/Fixed.hpp
@@ -0,0 +1,27 @@
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+#include <iostream>
+
+class Fixed
+{
+
+    private:
+
+        int         _value;
+        int const   _bit;
+
+    public:
+
+        Fixed();
+        Fixed(Fixed const & src);
+        ~Fixed();
+
+        Fixed &     operator=(Fixed const & rhs);
+
+        int         getRawBits(void) const;
+        void        setRawBits(int const raw);
+};
+
+
+#endif
---
+++ CPP/module02/ex01/Fixed.cpp
@@ -0,0 +1,45 @@
+#include "Fixed.hpp"
+
+Fixed::Fixed() : _value(0), _bit(8)
+{
+    std::cout << "Default Constructor Called" << std::endl;
+
+    return;
+}
+
+Fixed::~Fixed()
+{
+    std::cout << "Destructor Called" << std::endl;
+
+    return;
+}
+
+Fixed::Fixed(Fixed const & src) : _bit(8)
+{
+    std::cout << "Copy Constructor Called" << std::endl;
+
+    *this = src;
+    return;
+}
+
+Fixed &     Fixed::operator=(Fixed const & rhs) 
+{
+    std::cout << "Copy assignment operator called" << std::endl;
+
+    if (this != &rhs)
+        this->_value = rhs.getRawBits();
+
+    return (*this);
+}
+
+int         Fixed::getRawBits(void) const
+{
+    std::cout << "getRawBits member function Called" << std::endl;
+
+    return (this->_value);
+}
+
+void        Fixed::setRawBits(int const raw)
+{
+    this->_value = raw;
+}
